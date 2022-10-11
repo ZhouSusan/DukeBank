8733885c7a8140a13b210288b7ff00466943d995
ZhouSusan <szhou089@gmail.com>
2022-10-10 23:57:10 -0700

    created new bank object and a test user in main.java

--- src/main/java/org/example/ATM.java
+++ src/main/java/org/example/ATM.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 public class ATM {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
+        Bank dukeBank = new Bank();
+
+        //test user
+        User aUser = dukeBank.addUser("Steven", "Riggs", "1234", "10/31/2000", "1234567890");
     }
 }
