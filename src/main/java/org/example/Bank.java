df16298a46867768bf4623903fc48da3e00a1848
ZhouSusan <szhou089@gmail.com>
2022-10-10 21:12:03 -0700

    added getNewUserUUID(), which generates an new univseral unique id for a user

--- src/main/java/org/example/Bank.java
+++ src/main/java/org/example/Bank.java
@@ -15,6 +15,27 @@ public class Bank {
 
     public String getNewUserUUID() {
         String uuid;
-        Random rand = new Random;
+        Random rand = new Random();
+        int len = 8;
+        boolean isUnique;
+
+        //keep looping until we get a unique Id
+        do {
+            uuid = "";
+            for (int r = 0; r < len; r++) {
+                uuid += ((Integer)rand.nextInt(10)).toString();
+            }
+
+            //check to see if the user's id is unique
+            isUnique = false;
+            for (User u : this.users) {
+                if (uuid.compareTo(u.getUUID()) == 0) {
+                    isUnique = true;
+                    break;
+                }
+            }
+        } while(isUnique);
+        
+        return uuid;
     }
 }
