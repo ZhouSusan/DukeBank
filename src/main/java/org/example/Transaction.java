c8bee93b80aac20454c09dfda65dc656bf5647a3
ZhouSusan <szhou089@gmail.com>
2022-10-10 20:50:59 -0700

    created class Transaction with two constructors in Transaction.java

--- src/main/java/org/example/Transaction.java
+++ src/main/java/org/example/Transaction.java
@@ -3,4 +3,19 @@ import java.util.Date;
 
 public class Transaction {
     private Date timestamp;
+    private double amount;
+    private String memo;
+    private Account thisAccount;
+
+    public Transaction (double amount, Account thisAccount) {
+        this.amount = amount;
+        this.memo = "";
+        this.thisAccount = thisAccount;
+        this.timestamp = new Date();
+    }
+
+    public Transaction(double amount, Account thisAccount, String memo) {
+        this(amount, thisAccount);
+        this.memo = memo;
+    }
 }
