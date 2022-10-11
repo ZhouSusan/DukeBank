4a78852aa8ada510efaf8d528bd49f940fee763a
ZhouSusan <szhou089@gmail.com>
2022-10-10 20:59:01 -0700

    created class Bank, constructor, and imported java.util.ArrayList in Bank.java

--- src/main/java/org/example/Bank.java
+++ src/main/java/org/example/Bank.java
@@ -1,4 +1,14 @@
 package org.example;
+import java.util.ArrayList;
 
 public class Bank {
+    private String name;
+    private ArrayList<User> users;
+    private ArrayList<Account> accounts;
+
+    public Bank() {
+        this.name = "Duke Bank";
+        this.users = new ArrayList<User>();
+        this.accounts = new ArrayList<Account>();
+    }
 }
