05a38f4ac98685d58c63a418ee5153da9c44106e
ZhouSusan <szhou089@gmail.com>
2022-10-10 20:32:48 -0700

    created class Account and constructor in Account.java

---
+++ src/main/java/org/example/Account.java
@@ -0,0 +1,17 @@
+package org.example;
+import java.util.ArrayList;
+
+public class Account {
+    private String name;
+    //account id
+    private String uuid;
+    private User owner;
+    private ArrayList<Transaction> transactions;
+
+    public Account(String name, User owner, Bank dukeBank) {
+        this.name = name;
+        this.owner = owner;
+        this.uuid = dukeBank.getNewAccountUUID();
+        this.transactions = new ArrayList<Transaction>();
+    }
+}
