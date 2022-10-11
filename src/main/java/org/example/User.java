6aa6d2b438113ff380bedfeda8356223effad425
ZhouSusan <szhou089@gmail.com>
2022-10-10 20:17:42 -0700

    added constructor for User class in User.java

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,4 +1,5 @@
 package org.example;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.security.MessageDigest;
 
@@ -8,5 +9,27 @@ public class User {
     private String lastName;
     private byte pinEncypt[];
     private String dateOfBirth;
+    private String socialSecurityNumber;
     private ArrayList<Account> accounts;
+
+    public User(String firstName, String lastName, String pinNumber, String dateOfBirth, String socialSecurityNumber, Bank dukeBank) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.socialSecurityNumber = socialSecurityNumber;
+
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            this.pinEncypt = md.digest(pinNumber.getBytes());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+
+        //ToDo list: make this function
+        this.uuid = dukeBank.getNewUserUUID();
+
+        this.accounts = new ArrayList<Account>();
+
+        System.out.printf("A new user has been created successfully.\n%s, %s with ID# %s");
+    }
 }
